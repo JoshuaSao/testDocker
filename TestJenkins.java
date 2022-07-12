00e266ab6c5259a28cf460325212e39cbe9a4bab
Joshua <61245693+JoshuaSao@users.noreply.github.com>
2022-07-12 16:08:56 +0200

    Create TestJenkins.java

---
+++ TestJenkins.java
@@ -0,0 +1,9 @@
+public class TestJenkins {
+  public static void main(String[] args) {
+    System.out.println("Premier projet avec Docket Jenkins Java");
+       
+    for (int i = 0; i <= 5;  i++) {
+      System.out.println("Je suis la ligne :"+i);
+    } 
+  }
+}
